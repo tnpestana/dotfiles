0447a34d729c0c67792ec05bf11fc40df07c78e8
Tiago Pestana <tnevespestana@gmail.com>
2026-03-01 17:01:03 +0000

    update telescope version

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
+  version = '0.2.1',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local keymaps = require('core.keymaps')
