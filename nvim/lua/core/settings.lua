9dd6eb70b02ec4802b30089afe4852a864a28392
Tiago Pestana <tnevespestana@gmail.com>
2026-08-31 17:19:43 +0100

    auto-save and auto-reload settings

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -21,3 +21,50 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+-- Auto-reload files changed externally
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter' }, {
+  desc = 'Reload file when changed externally',
+  group = vim.api.nvim_create_augroup('auto-reload', { clear = true }),
+  callback = function()
+    vim.cmd('checktime')
+  end,
+})
+
+-- Auto-save files
+local save_timer = nil
+local function auto_save()
+  vim.schedule(function()
+    if
+      vim.bo.modified
+      and vim.bo.modifiable
+      and not vim.bo.readonly
+      and vim.fn.bufname('%') ~= ''
+      and not vim.tbl_contains({ 'neo-tree', 'lazy', 'mason' }, vim.bo.filetype)
+    then
+      vim.cmd('silent! write')
+    end
+  end)
+end
+
+local auto_save_group = vim.api.nvim_create_augroup('auto-save', { clear = true })
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+  desc = 'Auto-save on leaving insert mode',
+  group = auto_save_group,
+  callback = function()
+    auto_save()
+  end,
+})
+
+vim.api.nvim_create_autocmd('TextChanged', {
+  desc = 'Auto-save on text change (debounced)',
+  group = auto_save_group,
+  callback = function()
+    if save_timer then
+      save_timer:stop()
+    end
+    save_timer = vim.defer_fn(auto_save, 200)
+  end,
+})
